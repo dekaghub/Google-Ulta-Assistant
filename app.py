5f181d1a9a3667485dc5ffee8e83d771d28ffab6
Susanta <sdeka@bgsu.edu>
2019-10-27 10:06:37 -0400

    Insert LInk

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-
-
-
     return render_template('base.html')
+
+@app.route("/partial/")
+def partial():
+    return render_template("partial.html")
+
